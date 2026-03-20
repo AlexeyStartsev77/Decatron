ceb729ba4b7d3d4c1a94ebf246d55287693ecda6
AlexeyStartsev77 <88403750+AlexeyStartsev77@users.noreply.github.com>
2026-03-20 19:43:13 +0300

    Delete conv.h

--- Code_v_3/conv.h
+++
@@ -1,5 +0,0 @@
-String binar(uint8_t data) {
-    //String thisString = String(data, BIN);
-    String thisString = "00000000" + String(data, BIN);
-    return "0b" + thisString.substring(thisString.length()-8,thisString.length());
-}
